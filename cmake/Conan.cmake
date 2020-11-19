e6b2d37b18ae7989dd356864547a1e95f912b671
Brian Rader <brader@logikos.com>
2020-11-19 14:15:57 -0500

    AS-4: Update CMake File formatting.

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -15,15 +15,15 @@ macro(run_conan)
 
     conan_cmake_run(
         REQUIRES
-            ${CONAN_EXTRA_REQUIRES}
-            catch2/2.11.0
-            docopt.cpp/0.6.2
-            fmt/6.2.0
-            spdlog/1.5.0
+        ${CONAN_EXTRA_REQUIRES}
+        catch2/2.11.0
+        docopt.cpp/0.6.2
+        fmt/6.2.0
+        spdlog/1.5.0
         OPTIONS
-            ${CONAN_EXTRA_OPTIONS}
+        ${CONAN_EXTRA_OPTIONS}
         SETTINGS
-            compiler.cppstd=17
+        compiler.cppstd=17
         BASIC_SETUP
         CMAKE_TARGETS # individual targets to link to
         BUILD
